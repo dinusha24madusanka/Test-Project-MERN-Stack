638fea495b0b4e8e86d3eb9a57e3faa03385ebde
dinusha24madusanka <dinushamadusanka54@gmail.com>
2026-01-19 15:26:31 +0530

    2026.01.19
    Project 01
    MERN Stack
    Test the React.js

---
+++ testreactapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome Dinusha Madusanka</h1>
+        <button className='users-button'>Users</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
